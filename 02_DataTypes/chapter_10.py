6985026f85cbc842176eb39878fedfdd48c70ba3
Bharat <myprof219@gmail.com>
2025-07-21 23:26:08 +0530

    Set dict

---
+++ 02_DataTypes/chapter_10.py
@@ -0,0 +1,12 @@
+chai_order = dict(type="Masala Chai",size="Large" , sugar = 2)
+print(chai_order)
+
+chai_recepie = {}
+chai_recepie["base"] = "Black Tea"
+chai_recepie["Liquid"] = "Milk"
+
+print(chai_recepie)
+
+del chai_recepie["Liquid"]
+
+print(chai_recepie)
