cf979acdbf9085c1f8f1c8b584cbb5d3532adb89
Bharat <myprof219@gmail.com>
2025-07-22 23:59:14 +0530

    touched on advance datatypes like collections

--- 02_DataTypes/chapter_10.py
+++ 02_DataTypes/chapter_10.py
@@ -1,17 +1,17 @@
-chai_order = dict(type="Masala Chai",size="Large" , sugar = 2)
-print(chai_order)
+# chai_order = dict(type="Masala Chai",size="Large" , sugar = 2)
+# print(chai_order)
 
-chai_recepie = {}
-chai_recepie["base"] = "Black Tea"
-chai_recepie["Liquid"] = "Milk"
+# chai_recepie = {}
+# chai_recepie["base"] = "Black Tea"
+# chai_recepie["Liquid"] = "Milk"
 
-print(chai_recepie)
+# print(chai_recepie)
 
-del chai_recepie["Liquid"]
+# del chai_recepie["Liquid"]
 
-print(chai_recepie)
+# print(chai_recepie)
 
-print("sugar" in chai_order) 
-print(chai_order.keys())
-print(chai_order.values())
-print(chai_order.items())
+# print("sugar" in chai_order) 
+# print(chai_order.keys())
+# print(chai_order.values())
+# print(chai_order.items())
