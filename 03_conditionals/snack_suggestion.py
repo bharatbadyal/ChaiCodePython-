b2c782ef4d83f8473bf32b1e092a9525c6853c15
Bharat <myprof219@gmail.com>
2025-07-23 23:44:04 +0530

    conditionals

---
+++ 03_conditionals/snack_suggestion.py
@@ -0,0 +1,8 @@
+snack = input("Enter your preffered value: ").lower()
+
+# print(f"You Entred: {snack}")
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great Choice! We'll serve you {snack}")
+else:
+    print(f"{snack} is not available!") 
