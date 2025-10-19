f91c91e36d62d471b2173e3558313fc458c553f2
Bharat <myprof219@gmail.com>
2025-10-19 17:13:22 +0530

    Loops in Python

---
+++ 04_loops/02_batch_chai_prepration.py
@@ -0,0 +1,8 @@
+# The Chai Owner makes tea in batches every 15 minutes.
+# You want to simulate 4 Batches:
+# Task:
+# .. Use range() to simulate the batch no. 
+# .. Print: "Preparing chai for batch no #{number}"
+
+for batch in range(1,5):
+    print(f"Preparing chai for batch no #{batch}")
